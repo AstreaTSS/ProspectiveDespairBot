bf5c212b933dd7f872550262d24203b59c9bd397
Sonic4999 <sonictbm@gmail.com>
2021-06-08 09:16:05 -0400

    Attempt to fix auto announcements

--- cogs/auto_announcements.py
+++ cogs/auto_announcements.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 import importlib
 import os
@@ -5,9 +6,9 @@ import os
 import aiohttp
 import discord
 import pytz
+from dateutil.relativedelta import relativedelta
 from discord.ext import commands
 
-import common.backport_task as backport_task
 import common.utils as utils
 
 et = pytz.timezone("US/Eastern")
@@ -18,49 +19,54 @@ class AutoAnnouncements(commands.Cog):
         self.bot: commands.Bot = bot
         self.webhook = discord.Webhook.from_url(
             os.environ.get("WEBHOOK_URL"),
-            adapter=discord.AsyncWebhookAdapter(
-                aiohttp.ClientSession(loop=self.bot.loop)
-            ),
+            adapter=discord.AsyncWebhookAdapter(aiohttp.ClientSession()),
         )
 
-        self.auto_run.start()
+        self.task = self.bot.loop.create_task(self.auto_run())
 
     def cog_unload(self):
-        self.auto_run.cancel()
+        self.task.cancel()
 
-    @backport_task.loop(
-        time=(datetime.time(hour=9, tzinfo=et), datetime.time(hour=23, tzinfo=et))
-    )
     async def auto_run(self):
-        embed = discord.Embed(title="Announcement from Drake Aelius", color=11779669)
-        et_now = datetime.datetime.now(et)
+        while True:
+            et_now = datetime.datetime.now(et)
 
-        await utils.msg_to_owner(self.bot, et_now)
+            # very hacky way of finding out when to sleep based on the current time
+            if et_now.hour == 23:
+                sleep_till = et_now + relativedelta(
+                    days=+1, hour=9, minute=0, microsecond=0
+                )
+            elif 0 <= et_now.hour < 9:
+                sleep_till = et_now + relativedelta(hour=9, minute=0, microsecond=0)
+            else:
+                sleep_till = et_now + relativedelta(hour=23, minute=0, microsecond=0)
 
-        if et_now.hour == 23:
-            embed.description = """It's 11 PM. Go to sleep.
-            The mess hall will close in a few minutes. Move out of it quickly: it would be rather \
-            pathetic if you died just by being in it.
+            await discord.utils.sleep_until(sleep_till)
 
-            Remember: all you have to do to escape is kill... and I heard night time's the \
-            *perfect* time to kill."""
-        else:
-            embed.description = """Wake up, idiots.
-            It's now 9 AM. Unless you're a lazy ass, you should probably get ready for the day. \
-            And yeah, the mess hall has been unlocked too.
+            embed = discord.Embed(
+                title="Announcement from Drake Aelius", color=11779669
+            )
+            et_now = datetime.datetime.now(et)
 
-            Remember: if you want to escape, all you have to do is kill. If you can't convince \
-            yourself to do that... how weak."""
+            if et_now.hour == 23:
+                embed.description = """It's 11 PM. Go to sleep.
+                The mess hall will close in a few minutes. Move out of it quickly: it would be rather \
+                pathetic if you died just by being in it.
 
-        await self.webhook.send(embed=embed)
+                Remember: all you have to do to escape is kill... and I heard night time's the \
+                *perfect* time to kill."""
+            else:
+                embed.description = """Wake up, idiots.
+                It's now 9 AM. Unless you're a lazy ass, you should probably get ready for the day. \
+                And yeah, the mess hall has been unlocked too.
 
-    @auto_run.error
-    async def error_handle(self, *args):
-        error = args[-1]
-        await utils.error_handle(self.bot, error)
+                Remember: if you want to escape, all you have to do is kill. If you can't convince \
+                yourself to do that... how weak."""
+
+            await self.webhook.send(embed=embed)
+            await asyncio.sleep(3600)
 
 
 def setup(bot):
     importlib.reload(utils)
-    importlib.reload(backport_task)
     bot.add_cog(AutoAnnouncements(bot))
