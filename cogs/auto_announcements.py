939e8e2cb06b886c6ae3c3336454958888a292c7
Sonic4999 <sonictbm@gmail.com>
2021-09-06 20:00:11 -0400

    Added auto announcements for Season 3

--- cogs/auto_announcements.py
+++ cogs/auto_announcements.py
@@ -18,18 +18,17 @@ class AutoAnnouncements(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
 
-    #     self.webhook_session = aiohttp.ClientSession()
+        self.webhook_session = aiohttp.ClientSession()
 
-    #     self.webhook = discord.Webhook.from_url(
-    #         os.environ.get("WEBHOOK_URL"),
-    #         session=self.webhook_session,
-    #     )
+        self.webhook = discord.Webhook.from_url(
+            os.environ.get("WEBHOOK_URL"), session=self.webhook_session,
+        )
 
-    #     self.task = self.bot.loop.create_task(self.auto_run())
+        self.task = self.bot.loop.create_task(self.auto_run())
 
-    # def cog_unload(self):
-    #     self.task.cancel()
-    #     self.webhook_session.close()
+    def cog_unload(self):
+        self.task.cancel()
+        self.bot.loop.create_task(self.webhook_session.close())
 
     async def auto_run(self):
         while True:
@@ -52,25 +51,24 @@ class AutoAnnouncements(commands.Cog):
             await discord.utils.sleep_until(sleep_till)
 
             embed = discord.Embed(
-                title="Announcement from Drake Aelius", color=11779669
+                title="Announcement from Talia Aelius", color=0x155F60
             )
             et_now = datetime.datetime.now(et)
 
             if et_now.hour == 23:
                 str_builder = [
-                    "It's 11 PM. Go to sleep.\n",
-                    "The mess hall will close in a few minutes. Move out of it ",
-                    "quickly: it would be rather pathetic if you died just by being in it.",
-                    "\n\nRemember: all you have to do to escape is kill... ",
-                    "and I heard night time's the *perfect* time to kill.",
+                    "It's 11 PM, sweeties~ time to go to sleep!\n",
+                    "The cafeteria and kitchen will close in a few minutes. ",
+                    "I would move out of there if you don't want to be shot to ",
+                    "death~ it's not exciting as murder or being murder, after all!",
+                    "\n\nRemember: escape is only a murder away, especially at night~",
                 ]
             else:
                 str_builder = [
-                    "Wake up, idiots.\nIt's now 9 AM. Unless you're a lazy ass, ",
-                    "you should probably get ready for the day.\n",
-                    "And yeah, the mess hall has been unlocked too.",
-                    "\n\nRemember: if you want to escape, all you have to do is ",
-                    "kill. If you can't convince yourself to do that... how weak.",
+                    "Wake up, sweeties~ it's 9 AM!\n",
+                    "The cafeteria and kitchen are now open for your eating "
+                    "pleasure~ we wouldn't want to die on an empty stomach, would we?",
+                    "\n\nRemember: escape is only a murder away~",
                 ]
 
             embed.description = "".join(str_builder)
