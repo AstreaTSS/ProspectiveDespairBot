62bccfef97a4e24f56e340c3e4fc0dbc6b953764
Astrea <sonictbm@gmail.com>
2021-10-24 16:06:33 -0400

    :(

--- common/cards.py
+++ common/cards.py
@@ -112,6 +112,7 @@ fusanosuke_card = Card(
     oc_name="Fusanosuke Fukushima",
     oc_talent="Performer",
     card_url="https://cdn.discordapp.com/attachments/457939628209602560/883164243455787038/FusanosukeFukushimaCard.png",
+    status=Status.DEAD,
 )
 haru_card = Card(
     user_id=486181655837671425,
