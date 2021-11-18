53ee5af324503272738fc6cdad81aa7ea8bcc2c0
Sonic4999 <sonictbm@gmail.com>
2021-11-17 20:58:02 -0500

    Updated date for reference_date

--- cogs/card_handling.py
+++ cogs/card_handling.py
@@ -25,7 +25,7 @@ class CardHandling(commands.Cog, name="Card Handling"):
             def is_valid(m: discord.Message):
                 return m.author.id == self.bot.user.id
 
-            reference_date = datetime(2021, 6, 17)
+            reference_date = datetime(2021, 11, 17)
             await profile_chan.purge(limit=100, check=is_valid, after=reference_date)
 
             if cards.hosts:
